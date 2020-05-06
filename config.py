faaa66a93a61c4459a22d47ecaa1261a62745bb4
dennismwaniki67 <dennismwaniki67@gmail.com>
2020-05-06 11:31:44 +0300

    changed my config classes

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     General configuration parent class
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://dk:denno@localhost/pitch'
+#SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://dk:denno@localhost/pitch'
     UPLOADED_PHOTOS_DEST ='app/static'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     # email configurations
@@ -22,7 +22,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://dk:denno@localhost/pitch'
+    SQLALCHEMY_DATABASE_URI =os.environ.get("DATABASE_URL")
 
 class TestConfig(Config):
     '''
